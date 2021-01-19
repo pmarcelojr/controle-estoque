15b7d8a3f05e6e60c695cce13062eca046aedbea
pmarcelojr <marcelosantostecnologia@gmail.com>
2021-01-19 19:23:23 -0300

    Implementation navbar custom

--- ControleEstoque.Web/Controllers/HomeController.cs
+++ ControleEstoque.Web/Controllers/HomeController.cs
@@ -23,7 +23,7 @@ namespace ControleEstoque.Web.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
+        public IActionResult Sobre()
         {
             return View();
         }
